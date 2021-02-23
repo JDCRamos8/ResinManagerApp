2a0ef95254d8149998bccecdadb12cbd91248abf
Joshua Ramos <jdcramos8@gmail.com>
2021-02-23 13:53:37 -0800

    Skeleton code for set/when bot commands

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import discord
 
 from discord.ext import commands
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -15,9 +16,25 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
-# Max resin bot command
-@bot.command(amount='max' or '40')
-async def _max(ctx):
-    await ctx.send('You will have' + amount + 'resin in - hours and - minutes.')
+# Set: Sets resin amount and record time to calculate resin refill at specified amount
+@bot.command()
+async def set(ctx, arg):
+    global setTime, resin
+    setTime = datetime.now(tz=None)   # Get current time
+    resinAmount = arg
+
+    await ctx.send('Resin set to ' + arg + '!')
+
+# Check: Updates resin amount
+
+# When: Shows remaning time left for resin to refill at specified amount
+@bot.command()
+async def when(ctx, arg):
+    if arg == 'max':
+        d = datetime.now(tz=None)    
+        hr = str(d.hour)
+        min = str(d.minute)
+
+    await ctx.send('You will have ' + resinAmount + ' resin in ' + hr + ' hours and ' + min[-2] + ' minutes...')
 
 bot.run(TOKEN)
