02e88f6c080558fd6e0425207cb65fc43938338a
Joshua Ramos <jdcramos8@gmail.com>
2021-02-26 13:38:14 -0800

    Implemented help commands

--- bot.py
+++ bot.py
@@ -12,6 +12,9 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 # Bot, subclass of Client discord API
 bot = commands.Bot(command_prefix='!')
 
+# Remove default help command
+bot.remove_command("help")
+
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
@@ -35,6 +38,34 @@ async def when(ctx, arg):
         hr = str(d.hour)
         min = str(d.minute)
 
-    await ctx.send('You will have ' + resinAmount + ' resin in ' + hr + ' hours and ' + min[-2] + ' minutes...')
+    try:
+        await ctx.send('You will have ' + resinAmount + ' resin in ' + hr + ' hours and ' + min[-2] + ' minutes...')
+    except:
+        await ctx.send('No resin available. Use !set to input resin or !help for commands.')
+   
+# Help commands
+@bot.group(invoke_without_command = True)
+async def help(ctx):
+    emb = discord.Embed(title = "Help", description = "Use !help <command> for more info on a command.", color = ctx.author.color)
+    emb.add_field(name = "Resin" , value = "set, check, when")
+    await ctx.send(embed = emb)
+
+@help.command()
+async def set(ctx):
+    emb = discord.Embed(title = "Set", description = "Set current resin amount", color = ctx.author.color)
+    emb.add_field(name = "**Syntax**" , value = "!set <amount>")
+    await ctx.send(embed = emb)
+
+@help.command()
+async def check(ctx):
+    emb = discord.Embed(title = "Check", description = "Check current resin amount", color = ctx.author.color)
+    emb.add_field(name = "**Syntax**" , value = "!check")
+    await ctx.send(embed = emb)
+
+@help.command()
+async def when(ctx):
+    emb = discord.Embed(title = "When", description = "View remaining time until specified resin", color = ctx.author.color)
+    emb.add_field(name = "**Syntax**" , value = "!when <amount>")
+    await ctx.send(embed = emb)
 
 bot.run(TOKEN)
