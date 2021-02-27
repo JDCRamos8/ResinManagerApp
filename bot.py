eb38ea77ddf6e22a0d71c0fb3e4da4ecb1f0bdd0
Joshua Ramos <jdcramos8@gmail.com>
2021-02-27 15:47:22 -0800

    Implemented set and when methods

--- bot.py
+++ bot.py
@@ -1,71 +1,102 @@
 # bot.py
 import os
 import discord
-
-from discord.ext import commands
+import asyncio
+from discord.ext import tasks, commands
 from dotenv import load_dotenv
-from datetime import datetime
+from datetime import timedelta
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-# Bot, subclass of Client discord API
-bot = commands.Bot(command_prefix='!')
-
-# Remove default help command
-bot.remove_command("help")
+bot = commands.Bot(command_prefix='-')  # Bot, subclass of Client discord API
+bot.remove_command('help')              # Remove default help command
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+    activity = discord.Activity(name='your Resin', type=discord.ActivityType.watching)     # Bot status
+    await bot.change_presence(activity=activity)
+
+resinAmount = 0     # To be used as global variable
 
 # Set: Sets resin amount and record time to calculate resin refill at specified amount
 @bot.command()
-async def set(ctx, arg):
-    global setTime, resinAmount
-    setTime = datetime.now(tz=None)   # Get current time
+async def set(ctx, arg : int):
+    global resinAmount, count
     resinAmount = arg
 
-    await ctx.send('Resin set to ' + arg + '!')
+    if resinAmount in range (0,161):
+        @tasks.loop(seconds=3.0)      # Increase resinAmount every 8 minutes
+        async def count():
+            global resinAmount
+            await asyncio.sleep(5)
+            resinAmount += 1
+
+            if resinAmount == 160:    # Stop resin count and notifies user
+                channel = bot.get_channel(int(os.getenv('CHANNEL_ID')))
+                await channel.send('**Max Resin available!**')
+                count.stop()
+
+        count.start()                 # Begin resin count
+        await ctx.send('Resin set to ' + str(resinAmount) + '!')
+    else:
+       await ctx.send('Enter a valid Resin amount. (0-160)')
+
 
 # Check: Updates resin amount
+@bot.command()
+async def check(ctx):
+    try:
+        await ctx.send('You currently have ' + str(resinAmount) + ' Resin!')
+    except:
+        await ctx.send('No Resin available. Use !set to input Resin or !help for commands.')
+
 
 # When: Shows remaning time left for resin to refill at specified amount
+# Calculated by taking Resin arg - current Resin and multiplies it by 8 to find remaining time
 @bot.command()
-async def when(ctx, arg):
-    if arg == 'max':
-        d = datetime.now(tz=None)    
-        hr = str(d.hour)
-        min = str(d.minute)
+async def when(ctx, arg : int):
+    if arg < resinAmount:
+        await ctx.send('You already have ' + str(resinAmount) + ' Resin!')
+    elif arg in range (0,161):
+        result = arg - resinAmount
+        hr = str(timedelta(minutes=(result) * 8))[:2]
+        min = str(timedelta(minutes=(result) * 8))[3:5]
 
-    try:
-        await ctx.send('You will have ' + resinAmount + ' resin in ' + hr + ' hours and ' + min[-2] + ' minutes...')
-    except:
-        await ctx.send('No resin available. Use !set to input resin or !help for commands.')
+        await ctx.send('You will have %s Resin in %s hours and %s minutes.' % (str(arg), hr, min))
+    else:
+        await ctx.send('No Resin available. Use !set to input Resin or !help for commands.')
    
+
 # Help commands
 @bot.group(invoke_without_command = True)
 async def help(ctx):
-    emb = discord.Embed(title = "Help", description = "Use !help <command> for more info on a command.", color = ctx.author.color)
-    emb.add_field(name = "Resin" , value = "set, check, when")
+    emb = discord.Embed(title = 'Help', description = 'Use !help <command> for more info on a command.', color = ctx.author.color)
+    emb.add_field(name = 'Resin' , value = 'set, check, when')
     await ctx.send(embed = emb)
+
 
 @help.command()
 async def set(ctx):
-    emb = discord.Embed(title = "Set", description = "Set current resin amount", color = ctx.author.color)
-    emb.add_field(name = "**Syntax**" , value = "!set <amount>")
+    emb = discord.Embed(title = 'Set', description = 'Set current resin amount', color = ctx.author.color)
+    emb.add_field(name = '**Syntax**' , value = '!set <amount>')
     await ctx.send(embed = emb)
+
 
 @help.command()
 async def check(ctx):
-    emb = discord.Embed(title = "Check", description = "Check current resin amount", color = ctx.author.color)
-    emb.add_field(name = "**Syntax**" , value = "!check")
+    emb = discord.Embed(title = 'Check', description = 'Check current resin amount', color = ctx.author.color)
+    emb.add_field(name = '**Syntax**' , value = '!check')
     await ctx.send(embed = emb)
+
 
 @help.command()
 async def when(ctx):
-    emb = discord.Embed(title = "When", description = "View remaining time until specified resin", color = ctx.author.color)
-    emb.add_field(name = "**Syntax**" , value = "!when <amount>")
+    emb = discord.Embed(title = 'When', description = 'View remaining time until specified resin', color = ctx.author.color)
+    emb.add_field(name = '**Syntax**' , value = '!when <amount>')
     await ctx.send(embed = emb)
+
 
 bot.run(TOKEN)
